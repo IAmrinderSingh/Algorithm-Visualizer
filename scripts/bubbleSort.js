77e930db7f2cf946cf2453ba4825e2480caf8785
IAmrinderSingh <amrindersingh1703@gmail.com>
2022-04-13 10:29:45 +0530

    delay the execution to visualize

--- scripts/bubbleSort.js
+++ scripts/bubbleSort.js
@@ -1,3 +1,6 @@
+'use strict'
+const speed=500;
+const list_style=document.querySelector(".list-style");
 function swap(arr, xp, yp)
 {
 	var temp = arr[xp];
@@ -5,18 +8,28 @@ function swap(arr, xp, yp)
 	arr[yp] = temp;
 }
 
+function task(i,j,arr){
+	setTimeout(()=>{
+		if (arr[j] > arr[j+1])
+		{
+		const ele1=document.querySelector(`#A${j}`);
+		const ele2=document.querySelector(`#A${j+1}`);
+		ele1.setAttribute("style",`height:${arr[j+1]}px`);
+		ele2.setAttribute("style",`height:${arr[j]}px`);
+		swap(arr,j,j+1);
+		}
+	},speed*i)
+}
+
 // An optimized version of Bubble Sort
 function bubbleSort( arr, n)
 {
 var i, j;
 for (i = 0; i < n-1; i++)
-{
+{	
 	for (j = 0; j < n-i-1; j++)
-	{
-		if (arr[j] > arr[j+1])
-		{
-		swap(arr,j,j+1);
-		}
+	{	
+		task(i,j,arr);
 	}
 }
 }
