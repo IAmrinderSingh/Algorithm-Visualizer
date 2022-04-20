d024d5c80e3697340e9b72f103778d600d54d09a
IAmrinderSingh <amrindersingh1703@gmail.com>
2022-04-20 11:45:21 +0530

    importing merge sort

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,10 @@
-import {swap,bubbleSort} from "./bubbleSort.js"
+import {bubbleSort} from "./bubbleSort.js"
 import { generateArray ,list,max_value_arr,length_arr} from "./generateArray.js";
+import { mergeSort } from "./mergeSort.js";
 const bubble=document.querySelector("#bubble");
 const newarray=document.querySelector("#newarray");
 const algorithm=document.querySelector("#algorithm");
-
+const merge =document.querySelector('#merge');
 generateArray();
 
 newarray.addEventListener("click",()=>{
@@ -11,5 +12,9 @@ newarray.addEventListener("click",()=>{
 })
 
 bubble.addEventListener("click",()=>{
-    bubbleSort(list,length_arr);
+    bubbleSort();
+})
+
+merge.addEventListener("click",()=>{
+    mergeSort(list,0,length_arr-1);
 })
